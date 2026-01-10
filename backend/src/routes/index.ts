9f8a1c8c3839ce7a50573a2c1148532e931453f3
ivttsolutions <ivtt.solutions@gmail.com>
2026-01-10 19:42:23 +0100

    Add connections routes to index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import profileRoutes from './profile.routes';
 import postsRoutes from './posts.routes';
+import connectionsRoutes from './connections.routes';
 
 const router = Router();
 
@@ -20,5 +21,6 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/profile', profileRoutes);
 router.use('/posts', postsRoutes);
+router.use('/connections', connectionsRoutes);
 
 export default router;
