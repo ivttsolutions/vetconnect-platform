36eac0d32a0baa1123341a41e4e9c9ddc2206f38
ivttsolutions <ivtt.solutions@gmail.com>
2026-01-10 19:12:21 +0100

    Add global type declarations for multer

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="multer" />
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Multer.File;
+      files?: Multer.File[] | { [fieldname: string]: Multer.File[] };
+    }
+  }
+}
+
+export {};
